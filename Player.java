b8787ff9f9f6abffcab0d0ce1fffb9f7215be06f
dyqu1748 <dyqu1748@colorado.edu>
2020-11-08 13:45:47 -0700

    Update player attributes.
    Add player attributes for turn, retired, stocks, loans, degree, salary, insurance, and home. Implemented getters and setters for those attributes as well.

--- Player.java
+++ Player.java
@@ -3,13 +3,31 @@ import java.util.ArrayList;
 public class Player {
     private String name;
     private int money;
+    private int salary;
     private String job;
     private String spouse;
     private ArrayList<String> children;
     private int spaceLocation;
+    private boolean isTurn;
+    private boolean retired;
+    private int loans;
+    private int stocks;
+    private boolean carInsurance;
+    private boolean houseInsurance;
+    private String homeType;
+    private String degree;
 
     public Player(){
         this.money = 0;
+        this.salary = 0;
+        this.loans = 0;
+        this.stocks = 0;
+        this.isTurn = false;
+        this.retired = false;
+        this.carInsurance = false;
+        this.houseInsurance = false;
+        this.homeType = "";
+        this.degree = "";
         this.children = new ArrayList<String>();
     }
 
@@ -59,5 +77,79 @@ public class Player {
 
     public void setLocation(int location){
         this.spaceLocation = location;
+    }
+
+    public void setRetired(boolean ret){
+        this.retired = ret;
+    }
+
+    public boolean getRetired(){
+        return this.retired;
+    }
+
+    public void setIsTurn(boolean turn){
+        this.isTurn = turn;
+    }
+
+    public boolean getIsTurn(){
+        return this.isTurn;
+    }
+
+    public void setHomeType(String type){
+        this.homeType = type;
+    }
+
+    public String getHomeType(){
+        return this.homeType;
+    }
+
+    public void setDegree(String degree){
+        this.degree =degree;
+    }
+
+    public String getDegree(){
+        return this.degree;
+    }
+
+    //Loan and stock amounts will be a constant; attributes will track how many of each the player has
+    public void setLoans(){
+        this.loans++;
+    }
+
+    public int getLoans(){
+        return this.loans;
+    }
+
+    public void setStocks(){
+        this.stocks++;
+    }
+
+    public int getStocks(){
+        return this.stocks;
+    }
+
+    public void setCarInsurance(boolean bought){
+        this.carInsurance = bought;
+    }
+
+    public boolean getCarInsurance(){
+        return this.carInsurance;
+    }
+
+    public void setHouseInsurance(boolean bought){
+        this.houseInsurance = bought;
+    }
+
+    public boolean getHouseInsurance(){
+        return this.houseInsurance;
+    }
+
+
+    public void setSalary(int salary){
+        this.salary = salary;
+    }
+
+    public int getSalary(){
+        return this.salary;
     }
 }
